a94699a1d96fb0896da4fdb719f9b157b147e0b8
ascherbakov-ya@yandex.ru <ascherbakov-ya@yandex.ru>
2016-04-10 21:44:22 +0300

    .

--- usageresources/views.py
+++ usageresources/views.py
@@ -165,3 +165,7 @@ class IndexView(tables.DataTableView):
 
             return context
 
+    def median(numbers):
+        return (sorted(numbers)[int(round((len(numbers) - 1) / 2.0))] + sorted(numbers)[int(round((len(numbers) - 1) // 2.0))]) / 2.0
+
+
