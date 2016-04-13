f55f3059c18e4eb314df754f5bf8c0e220098af7
ascherbakov-ya@yandex.ru <ascherbakov-ya@yandex.ru>
2016-04-14 01:37:10 +0300

    .

--- usageresources/views.py
+++ usageresources/views.py
@@ -25,7 +25,7 @@ class IndexView(tables.DataTableView):
 
     def get_data(self):
             instances, has_more0 = api.nova.server_list(self.request,all_tenants=True)
-            tenant_list, has_more1  = api.keystone.tenant_list(self.request)
+            domain_list  = api.keystone.domain_list(self.request)
             ceiloclient = cc(self.request)
             hypervisor_list = api.nova.hypervisor_list(self.request)
             flavors = api.nova.flavor_list(self.request)
@@ -65,91 +65,108 @@ class IndexView(tables.DataTableView):
 
             self.hvs_load_avg.append([hvs_load_avg_tmp])
 
-            for tenant in tenant_list:
+            self.ten_list = []
 
-                quota_data = quotas.get_tenant_quota_data(self.request, tenant_id=tenant.id)
-                project = { "cpu_util": 0.0, "cpu_flavored": 0, "memory_util": 0, "memory_flavored": 0, "disk_util": 0, "disk_flavored": 0 }
+            for domain in domain_list:
 
-                for field in ("cores","ram","gigabytes"):
-                    qs[str(field)] = quota_data.get(field).limit
+              domain_context = self.request.session.get('domain_context', None)
 
-                for instance in instances:
-                    if instance and instance.tenant_id == tenant.id:
-                     query = [dict(field='resource_id', op='eq', value = instance.id)]
-                     cpu = ceiloclient.samples.list(meter_name='cpu_util', limit=1, q=query)
-                     mem = ceiloclient.samples.list(meter_name='memory.resident', limit=1, q=query)
-                     disk = ceiloclient.samples.list(meter_name='disk.allocation', limit=1, q=query)
+              tenant_list, has_more1  = api.keystone.tenant_list(self.request, domain=domain_context)
 
-                     flavor_id = instance.flavor["id"]
-                     if flavor_id in full_flavors:
-                        instance.full_flavor = full_flavors[flavor_id]
-                     else:
-                        instance.full_flavor = api.nova.flavor_get(self.request, flavor_id)
+              if tenant_list:
 
-                     try:
-                        project['cpu_flavored'] += instance.full_flavor.vcpus
-                        totals['vcpus_flavored'] += instance.full_flavor.vcpus
-                        project['memory_flavored'] += instance.full_flavor.ram
-                        project['disk_flavored'] += instance.full_flavor.disk
-                     except:
-                        LOG.error("Error flavor for instance: %s" % instance.id)
+               for tenant in tenant_list:
 
-                     try:
-                        project['cpu_util'] += cpu[0].counter_volume * instance.full_flavor.vcpus
-                        totals['vcpu_util'] += project['cpu_util']
-                        project['memory_util'] += mem[0].counter_volume
-                        totals['memory_util'] += project['memory_util']
-                        project['disk_util'] += disk[0].counter_volume*0.000000001024
-                     except:
-                        LOG.error("Error ceilometer for instance: %s" % instance.id)
+                   quota_data = quotas.get_tenant_quota_data(self.request, tenant_id=tenant.id)
+                   project = { "cpu_util": 0.0, "cpu_flavored": 0, "memory_util": 0, "memory_flavored": 0, "disk_util": 0, "disk_flavored": 0 }
+
+                   for field in ("cores","ram","gigabytes"):
+                       qs[str(field)] = quota_data.get(field).limit
+
+                   for instance in instances:
+                       if instance and instance.tenant_id == tenant.id:
+                          query = [dict(field='resource_id', op='eq', value = instance.id)]
+                          cpu = ceiloclient.samples.list(meter_name='cpu_util', limit=1, q=query)
+                          mem = ceiloclient.samples.list(meter_name='memory.resident', limit=1, q=query)
+                          disk = ceiloclient.samples.list(meter_name='disk.allocation', limit=1, q=query)
+
+                          flavor_id = instance.flavor["id"]
+                          if flavor_id in full_flavors:
+                             instance.full_flavor = full_flavors[flavor_id]
+                          else:
+                             instance.full_flavor = api.nova.flavor_get(self.request, flavor_id)
+
+                          try:
+                             project['cpu_flavored'] += instance.full_flavor.vcpus
+                             totals['vcpus_flavored'] += instance.full_flavor.vcpus
+                             project['memory_flavored'] += instance.full_flavor.ram
+                             project['disk_flavored'] += instance.full_flavor.disk
+                          except:
+                             LOG.error("Error flavor for instance: %s" % instance.id)
+
+                          try:
+                             project['cpu_util'] += cpu[0].counter_volume * instance.full_flavor.vcpus
+                             totals['vcpu_util'] += project['cpu_util']
+                             project['memory_util'] += mem[0].counter_volume
+                             totals['memory_util'] += project['memory_util']
+                             project['disk_util'] += disk[0].counter_volume*0.000000001024
+                          except:
+                             LOG.error("Error ceilometer for instance: %s" % instance.id)
 
 
-                cpu_util_avg_tmp['cpu_util'].append(project['cpu_util'])
-                mem_util_avg_tmp['memory_util'].append(project['memory_util'])
+                   cpu_util_avg_tmp['cpu_util'].append(project['cpu_util'])
+                   mem_util_avg_tmp['memory_util'].append(project['memory_util'])
 
-                tenant.cpu_util = lambda:'-'
-                setattr(tenant,"cpu_util", round(project['cpu_util'],3))
-                tenant.memory_util = lambda:'-'
-                setattr(tenant,"memory_util", round(project['memory_util'],3))
-                tenant.disk_util = lambda:'-'
-                setattr(tenant,"disk_util", round(project['disk_util'],3))
+                   tenant.cpu_util = lambda:'-'
+                   setattr(tenant,"cpu_util", round(project['cpu_util'],3))
+                   tenant.memory_util = lambda:'-'
+                   setattr(tenant,"memory_util", round(project['memory_util'],3))
+                   tenant.disk_util = lambda:'-'
+                   setattr(tenant,"disk_util", round(project['disk_util'],3))
 
-                tenant.cpu_flavored = lambda:'-'
-                setattr(tenant,"cpu_flavored",project['cpu_flavored'])
-                tenant.memory_flavored = lambda:'-'
-                setattr(tenant,"memory_flavored",project['memory_flavored'])
-                tenant.disk_flavored = lambda:'-'
-                setattr(tenant,"disk_flavored",project['disk_flavored'])
+                   tenant.cpu_flavored = lambda:'-'
+                   setattr(tenant,"cpu_flavored",project['cpu_flavored'])
+                   tenant.memory_flavored = lambda:'-'
+                   setattr(tenant,"memory_flavored",project['memory_flavored'])
+                   tenant.disk_flavored = lambda:'-'
+                   setattr(tenant,"disk_flavored",project['disk_flavored'])
 
-                tenant.cpu_quota = lambda:'-'
-                setattr(tenant,"cpu_quota",qs['cores'])
-                tenant.memory_quota = lambda:'-'
-                setattr(tenant,"memory_quota",qs['ram'])
-                tenant.disk_quota = lambda:'-'
-                setattr(tenant,"disk_quota",qs['gigabytes'])
+                   tenant.cpu_quota = lambda:'-'
+                   setattr(tenant,"cpu_quota",qs['cores'])
+                   tenant.memory_quota = lambda:'-'
+                   setattr(tenant,"memory_quota",qs['ram'])
+                   tenant.disk_quota = lambda:'-'
+                   setattr(tenant,"disk_quota",qs['gigabytes'])
 
-                tenant.cpu_total = lambda:'-'
-                setattr(tenant,"cpu_total",totals['vcpus'])
-                tenant.memory_total = lambda:'-'
-                setattr(tenant,"memory_total",totals['memory_mb'])
-                tenant.disk_total = lambda:'-'
-                setattr(tenant,"disk_total",totals['local_gb'])
+                   tenant.cpu_total = lambda:'-'
+                   setattr(tenant,"cpu_total",totals['vcpus'])
+                   tenant.memory_total = lambda:'-'
+                   setattr(tenant,"memory_total",totals['memory_mb'])
+                   tenant.disk_total = lambda:'-'
+                   setattr(tenant,"disk_total",totals['local_gb'])
 
-            self.ten_list = tenant_list
+               #self.ten_list = tenant_list
+               self.ten_list += tenant_list
 
-            cpu_util_avg_tmp['date'] = int(time.time())
-            mem_util_avg_tmp['date'] = int(time.time())
-            cpu_util_avg_tmp['cpu_util_avg'] = 100 / ((totals['vcpus_flavored'] * 100) / totals['vcpu_util'])
-            mem_util_avg_tmp['memory_util_avg'] = 100 / (totals['memory_mb'] / totals['memory_util'])
+               cpu_util_avg_tmp['date'] = int(time.time())
+               mem_util_avg_tmp['date'] = int(time.time())
+               try:
+                  cpu_util_avg_tmp['cpu_util_avg'] = 100 / ((totals['vcpus_flavored'] * 100) / totals['vcpu_util'])
+                  mem_util_avg_tmp['memory_util_avg'] = 100 / (totals['memory_mb'] / totals['memory_util'])
+               except:
+                  pass
 
-            LOG.info(totals)
-            LOG.info(cpu_util_avg_tmp)
-            LOG.info(mem_util_avg_tmp)
+               LOG.info(totals)
+               LOG.info(cpu_util_avg_tmp)
+               LOG.info(mem_util_avg_tmp)
 
-            self.cpu_util_avg.append([{"date":cpu_util_avg_tmp['date'],"cpu":cpu_util_avg_tmp['cpu_util_avg']}])
-            self.mem_util_avg.append([{"date":mem_util_avg_tmp['date'],"memory":mem_util_avg_tmp['memory_util_avg']}])
+               try:
+                  self.cpu_util_avg.append([{"date":cpu_util_avg_tmp['date'],"cpu":cpu_util_avg_tmp['cpu_util_avg']}])
+                  self.mem_util_avg.append([{"date":mem_util_avg_tmp['date'],"memory":mem_util_avg_tmp['memory_util_avg']}])
+               except:
+                  pass
 
-            return tenant_list
+               return self.ten_list
 
     def get_context_data(self):
             context = super(IndexView, self).get_context_data()
